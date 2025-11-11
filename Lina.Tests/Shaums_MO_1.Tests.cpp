dbd99ffea5f4fc5528c15a0708a39ff186c48caf
Erik Baklund <eb@fjordmaritime.no>
2025-11-11 18:27:48 +0100

    Done MO_1_3

--- Lina.Tests/Shaums_MO_1.Tests.cpp
+++ Lina.Tests/Shaums_MO_1.Tests.cpp
@@ -96,5 +96,26 @@ namespace LinaTests
             Assert::AreEqual(A * B == C1, true);
             Assert::AreEqual(B * A == C2, true);
         }
+
+        TEST_METHOD(MO_1_5)
+        {
+            Mtx<float, 2, 3> A({
+                1.f,   2.f, 3.f,
+                4.f,  -5.f, 6.f
+            });
+            Mtx<float, 2, 2> B({
+                7.f,   8.f,
+                0.f,  -9.f
+            });
+            Mtx<float, 2, 3> C({
+                 39.f, -26.f,  69.f,
+                -36.f,  45.f, -54.f
+            });
+
+
+            // Works! - Does not compile :)
+            // Assert::AreEqual(A * B == C, true);
+            Assert::AreEqual(B * A == C, true);
+        }
     };
 }
